341dcc207e237575a439505772e31c8103c2e33b
Niklas-gr-dot <76098961+Niklas-gr-dot@users.noreply.github.com>
2022-01-07 15:39:05 +0100

    Update core.py

--- tsai/callback/core.py
+++ tsai/callback/core.py
@@ -99,7 +99,7 @@ class ShowGraph(Callback):
             self.rec_start = len(rec.losses)
         iters = range_of(rec.losses)
         val_losses = np.stack(rec.values)[:, self.learn.recorder.loss_idxs[-1]].tolist()
-        print("Hello", val_losses)
+        
         x_bounds = (0, len(rec.losses) - 1)
         if self.epoch == 0:
             y_min = min((min(rec.losses), min(val_losses)))
@@ -131,7 +131,7 @@ class ShowGraph(Callback):
         self.graph_ax.set_title(f'Losses\nepoch: {self.epoch +1}/{self.n_epoch}')
         self.graph_out.update(self.graph_ax.figure)
 
-#ShowGraphCallback2 = ShowGraph
+ShowGraphCallback2 = ShowGraph
 
 # Cell
 
