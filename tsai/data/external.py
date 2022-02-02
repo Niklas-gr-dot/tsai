97c13e8f329ba9d305d890a5686996e1a7ac8f4f
Niklas-gr-dot <76098961+Niklas-gr-dot@users.noreply.github.com>
2022-02-02 08:16:27 +0100

    import error

--- tsai/data/external.py
+++ tsai/data/external.py
@@ -14,9 +14,9 @@ from ..utils import *
 from .validation import *
 
 # Cell
-from sktime.utils.data_io import load_from_tsfile_to_dataframe as ts2df
+#from sktime.utils.data_io import load_from_tsfile_to_dataframe as ts2df
 from sktime.utils.validation.panel import check_X
-from sktime.utils.data_io import TsFileParseException
+#from sktime.utils.data_io import TsFileParseException
 
 # Cell
 from fastai.data.external import *
