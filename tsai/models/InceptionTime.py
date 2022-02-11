abcb9e329b50ababc23669e3c504e75e424acd96
Niklas-gr-dot <76098961+Niklas-gr-dot@users.noreply.github.com>
2022-02-11 20:35:17 +0100

    Numberfilters 10 statt 32, prints

--- tsai/models/InceptionTime.py
+++ tsai/models/InceptionTime.py
@@ -35,7 +35,7 @@ class InceptionModule(Module):
 
 @delegates(InceptionModule.__init__)
 class InceptionBlock(Module):
-    def __init__(self, ni, nf=32, residual=True, depth=6, **kwargs):
+    def __init__(self, ni, nf=10, residual=True, depth=6, **kwargs):
         self.residual, self.depth = residual, depth
         self.inception, self.shortcut = nn.ModuleList(), nn.ModuleList()
         for d in range(depth):
@@ -45,7 +45,9 @@ class InceptionBlock(Module):
                 self.shortcut.append(BN1d(n_in) if n_in == n_out else ConvBlock(n_in, n_out, 1, act=None))
         self.add = Add()
         self.act = nn.ReLU()
-
+        print("Activation function: ",self.act)
+        print("Number of filters: ", nf)
+        print("depth (Number of InceptionModules) ", depth)
     def forward(self, x):
         res = x
         for d, l in enumerate(range(self.depth)):
