3ca401d8742ba1ff1b88626a1c81b19785e5520f
Niklas-gr-dot <76098961+Niklas-gr-dot@users.noreply.github.com>
2022-02-26 11:25:35 +0100

    depth 10

--- tsai/models/InceptionTime.py
+++ tsai/models/InceptionTime.py
@@ -35,7 +35,7 @@ class InceptionModule(Module):
 
 @delegates(InceptionModule.__init__)
 class InceptionBlock(Module):
-    def __init__(self, ni, nf=32, residual=True, depth=6, **kwargs):
+    def __init__(self, ni, nf=32, residual=True, depth=10, **kwargs):
         self.residual, self.depth = residual, depth
         self.inception, self.shortcut = nn.ModuleList(), nn.ModuleList()
         for d in range(depth):
