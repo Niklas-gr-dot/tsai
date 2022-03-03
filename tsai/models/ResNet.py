8f9888ab64529f2a02bf6c2407668ff9d53527ce
Niklas-gr-dot <76098961+Niklas-gr-dot@users.noreply.github.com>
2022-03-03 08:30:11 +0100

    activation TanH

--- tsai/models/ResNet.py
+++ tsai/models/ResNet.py
@@ -17,7 +17,7 @@ class ResBlock(Module):
         # expand channels for the sum if necessary
         self.shortcut = BN1d(ni) if ni == nf else ConvBlock(ni, nf, 1, act=None)
         self.add = Add()
-        self.act = nn.Sigmoid()
+        self.act = nn.Tanh()
 
     def forward(self, x):
         res = x
