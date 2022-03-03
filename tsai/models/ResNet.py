326b26055dc8eaf997bdf7ed8bba6ff4ca18ccb7
Niklas-gr-dot <76098961+Niklas-gr-dot@users.noreply.github.com>
2022-03-04 00:19:38 +0100

    lr 0.0001, relu

--- tsai/models/ResNet.py
+++ tsai/models/ResNet.py
@@ -17,7 +17,7 @@ class ResBlock(Module):
         # expand channels for the sum if necessary
         self.shortcut = BN1d(ni) if ni == nf else ConvBlock(ni, nf, 1, act=None)
         self.add = Add()
-        self.act = nn.Tanh()
+        self.act = nn.ReLU()
 
     def forward(self, x):
         res = x
