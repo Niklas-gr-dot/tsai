9d4410211da8bf99ff6654a1c5d35178a55659e3
Niklas-gr-dot <76098961+Niklas-gr-dot@users.noreply.github.com>
2022-03-05 08:34:32 +0100

    nf 10

--- tsai/models/ResNet.py
+++ tsai/models/ResNet.py
@@ -30,7 +30,7 @@ class ResBlock(Module):
 
 class ResNet(Module):
     def __init__(self, c_in, c_out):
-        nf = 64
+        nf = 10
         kss=[7, 5, 3]
         print("number of filters: ", nf)
         print("Kernelsizes  : ", kss)
