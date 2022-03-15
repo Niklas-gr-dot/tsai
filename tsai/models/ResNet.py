cb30f6f35a1402bc9926862da93524df0864e049
Niklas-gr-dot <76098961+Niklas-gr-dot@users.noreply.github.com>
2022-03-15 12:15:42 +0100

    5.3.1 kss

--- tsai/models/ResNet.py
+++ tsai/models/ResNet.py
@@ -8,15 +8,13 @@ from .layers import *
 
 # Cell
 class ResBlock(Module):
-    def __init__(self, ni, nf, kss=[13,11,9,7, 5, 3,1]):
+    def __init__(self, ni, nf, kss=[ 5, 3,1]):
         
         self.convblock1 = ConvBlock(ni, nf, kss[0])
         self.convblock2 = ConvBlock(nf, nf, kss[1])
-        self.convblock3 = ConvBlock(nf, nf, kss[2])
-        self.convblock4 = ConvBlock(nf, nf, kss[3])
-        self.convblock5 = ConvBlock(nf, nf, kss[4])
-        self.convblock6 = ConvBlock(nf, nf, kss[5])
-        self.convblock7 = ConvBlock(nf, nf, kss[6], act=None)
+        self.convblock3 = ConvBlock(nf, nf, kss[2], act=None)
+        
+        
 
         # expand channels for the sum if necessary
         self.shortcut = BN1d(ni) if ni == nf else ConvBlock(ni, nf, 1, act=None)
@@ -29,10 +27,7 @@ class ResBlock(Module):
         x = self.convblock2(x)
         x = self.convblock3(x)
         #####
-        x = self.convblock4(x)
-        x = self.convblock5(x)
-        x = self.convblock6(x)
-        x = self.convblock7(x)
+       
         x = self.add(x, self.shortcut(res))
         x = self.act(x)
         return x
@@ -40,7 +35,7 @@ class ResBlock(Module):
 class ResNet(Module):
     def __init__(self, c_in, c_out):
         nf = 64
-        kss=[13,11,9,7, 5, 3,1]
+        kss=[ 5, 3,1]
         print("number of filters: ", nf)
         print("Kernelsizes  : ", kss)
         self.resblock1 = ResBlock(c_in, nf, kss=kss)
